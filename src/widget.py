5be733a40a9f4fdb60a05ed4f4c77da559209bb2
ildar_safarov <ildarsafarov555@gmail.com>
2024-12-05 17:54:05 +0300

    Добавлен модуль widget с функциями mask_account_card и get_date

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,32 @@
-def get_mask_card_number(card_number: str) -> str:
-    """Маскирует номер банковской карты."""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
-def get_mask_account(account_number: str) -> str:
-    """Маскирует номер банковского счета."""
-    return f"**{account_number[-4:]}"
+def mask_account_card(data: str) -> str:
+    """
+    Маскирует номер карты или счета.
+
+    :param data: строка вида "Visa Platinum 7000792289606361" или "Счет 73654108430135874305"
+    :return: строка с замаскированным номером
+    """
+    if data.lower().startswith("счет"):
+        # Обработка счета
+        account_number = data.split(maxsplit=1)[1]
+        return f"Счет {get_mask_account(account_number)}"
+    else:
+        # Обработка карты
+        card_type, card_number = data.rsplit(maxsplit=1)
+        return f"{card_type} {get_mask_card_number(card_number)}"
+
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует дату из формата "2024-03-11T02:26:18.671407" в формат "ДД.ММ.ГГГГ".
+
+    :param date_str: строка с датой в ISO формате
+    :return: строка с датой в формате "ДД.ММ.ГГГГ"
+    """
+
+    date_obj = datetime.fromisoformat(date_str)
+    return date_obj.strftime("%d.%m.%Y")
