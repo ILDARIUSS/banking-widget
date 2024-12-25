c0637d14aae017647fcd667326d62163a1b26832
ildar_safarov <ildarsafarov555@gmail.com>
2024-12-25 17:46:18 +0300

    All new files fixed again

--- src/generators.py
+++ src/generators.py
@@ -1,38 +1,23 @@
-from typing import Dict, Iterator, List
-
-
-def filter_by_currency(transactions: List[Dict], currency_code: str) -> Iterator[Dict]:
+def filter_by_currency(transactions: list[dict], currency_code: str):
     """
-    Генератор для фильтрации транзакций по указанной валюте.
-
-    :param transactions: Список словарей с транзакциями.
-    :param currency_code: Код валюты, например, "USD".
-    :yield: Транзакции, где валюта совпадает с currency_code.
+    Фильтрует транзакции по заданной валюте.
     """
     for transaction in transactions:
-        currency = transaction.get("operationAmount", {}).get("currency", {}).get("code")
-        if currency == currency_code:
+        if transaction["operationAmount"]["currency"]["code"] == currency_code:
             yield transaction
 
 
-def transaction_descriptions(transactions: List[Dict]) -> Iterator[str]:
+def transaction_descriptions(transactions: list[dict]):
     """
-    Генератор, возвращающий описание каждой транзакции.
-
-    :param transactions: Список словарей с транзакциями.
-    :yield: Описание транзакции, если оно есть, иначе строка "Описание отсутствует".
+    Возвращает описания транзакций.
     """
     for transaction in transactions:
-        yield transaction.get("description", "Описание отсутствует")
+        yield transaction["description"]
 
 
-def card_number_generator(start: int, end: int) -> Iterator[str]:
+def card_number_generator(start: int, end: int):
     """
-    Генератор номеров банковских карт в формате XXXX XXXX XXXX XXXX.
-
-    :param start: Начальное значение диапазона (включительно).
-    :param end: Конечное значение диапазона (включительно).
-    :yield: Номер карты в формате XXXX XXXX XXXX XXXX.
+    Генерирует номера карт в заданном диапазоне.
     """
     for number in range(start, end + 1):
-        yield f"{number:016d}".replace("", " ")[1:-1].strip()
+        yield f"{number:016}"
